e55f48d46d73d346d8f68727fd1f1ce6a76b64c6
Alexei Ptitchkin <ptitchkin@gmail.com>
2022-01-09 19:28:01 -0500

    downsize logo image

--- views/logo.py
+++ views/logo.py
@@ -9,7 +9,7 @@ class Logo:
 
     def __init__(self, rootCanvas):
         logo = Image.open('./assets/logoTransp.png')
-        logo = resize_image(logo, 400, 400)
+        logo = resize_image(logo, 200, 200)
         logo = ImageTk.PhotoImage(logo)
         logo_label = tk.Label(rootCanvas, image=logo)
         logo_label.image = logo
